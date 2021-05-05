94f11e360a167b63d2277e97a8f4432f28a2d49c
Dawid <51885195+Dstaary@users.noreply.github.com>
2021-05-05 23:43:16 +0200

    fix

--- burgermenu.js
+++ burgermenu.js
@@ -1,6 +1,12 @@
 const burger = document.querySelector('div.hamburger');
 const menu = document.querySelector('ul.menu-items');
+const menuList = document.querySelectorAll('ul.menu-items > li');
 
 burger.addEventListener("click", () => {
-  menu.classList.toggle("show_menu")
+  menu.classList.toggle("show_menu");
+  menuList.forEach((li) => {
+    li.onclick = function(){
+      menu.classList.remove("show_menu");
+  }
+  })
 })
